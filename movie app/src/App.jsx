8f06b0a67b489eddca3ccee80915da9e83a85a25
WeThinkCode Student <keutlwilempolokeng@gmail.com>
2025-04-03 22:37:11 +0200

    app stated

--- movie app/src/App.jsx
+++ movie app/src/App.jsx
@@ -3,32 +3,30 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Movies({movie}){
+  const [liked , setLiked] = useState(false)
+ 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {movie}
+      <button onClick={
+        () => {
+          setLiked(true)
+        }
+      }
+      >{ liked? 'liked' : 'like'}</button>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Movies movie= "one"/>
+      <Movies movie= "2"/>
+      <Movies movie= "3"/>
+
+    </div>
   )
 }
 
