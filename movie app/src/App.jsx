27f92d946b75bb9a66ac5459b1e6695ceb2ed55b
WeThinkCode Student <keutlwilempolokeng@gmail.com>
2025-04-05 15:01:24 +0200

    .

--- movie app/src/App.jsx
+++ movie app/src/App.jsx
@@ -4,8 +4,12 @@ function App(){
 
   return (
     <main>
+
+      <div className='pattern'/>
+      
       <div className='wrapper'>
         <header>
+          <img src="/src/images/hero_BG.png" alt="heros img" />
           <h1>We bring <span className='text-gradient'> Great Movies</span> to watch!</h1>
         </header>
      </div>
